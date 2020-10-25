d259ff86dbefce2a679d2c5f9bf32302716668f7
Aubrey Sledzinski <asledzinski88@gmail.com>
2020-10-25 16:20:57 -0400

    React Router

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import './App.scss';
 import LaunchCard  from './components/Launches/LaunchCard/LaunchCard.jsx';
 import NavBar from './components/navbar/NavBar';
 import PastLaunches from './components/Launches/pastLaunches/PastLaunches';
+import PastLaunchesFull from './components/Launches/pastLaunches/pastLaunchesFull/PastLaunchesFull';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
 
 
 function App() {
 
   return (
+    <Router>
     <div className="App">
       <NavBar/>
-      <LaunchCard/>
-      <PastLaunches/>
+      <Switch>
+      <Route exact path="/">
+        <LaunchCard/>
+        <PastLaunches/>
+      
+      </Route>
+      <Route path="/pastLaunchFull">
+          <PastLaunchesFull/>
+      </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/Launches/pastLaunches/pastLaunchesFull/PastLaunchesFull.jsx
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from 'react';
+
+function PastLaunchesFull(props) {
+    console.log("PASTFULL",props);
+
+    const [flightNumber, setFlightNumber] = useState();
+
+  
+
+
+    return (
+        <h1>Past Full Page</h1>
+    );
+
+
+}
+
+export default PastLaunchesFull;
